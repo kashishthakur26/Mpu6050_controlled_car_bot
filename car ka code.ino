7a20b2e15a6be86fd78d85ed68101c46bab2c31e
kashishthakur26 <120331369+kashishthakur26@users.noreply.github.com>
2023-04-08 12:26:38 +0530

    Add files via upload

---
+++ car ka code.ino
@@ -0,0 +1,88 @@
+#include<ESP8266WiFi.h>
+#include<ESP8266WebServer.h>
+
+IPAddress ip(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+IPAddress gateway(192,168,4,1);
+
+const char *ssid="ESPap";
+const char *password="12345678";
+
+ESP8266WebServer server(80);
+
+void handleRoot(){
+  server.send(200,"text/html","Welcome To ErodeRTP");
+  digitalWrite(D5,LOW);
+  digitalWrite(D6,LOW); 
+  
+  digitalWrite(D7,LOW);
+  digitalWrite(D8,LOW); 
+}
+
+void halt(){
+  server.send(200,"text/html","HALT");
+  digitalWrite(D5,LOW);
+  digitalWrite(D6,LOW);
+  
+  digitalWrite(D7,LOW);
+  digitalWrite(D8,LOW);
+}
+
+void forward(){
+  server.send(200,"text/html","FORWARD");
+  digitalWrite(D5,HIGH);
+  digitalWrite(D6,LOW); 
+  
+  digitalWrite(D7,HIGH);
+  digitalWrite(D8,LOW); 
+}
+
+void backward(){
+  server.send(200,"text/html","BACKWARD");
+  digitalWrite(D5,LOW);
+  digitalWrite(D6,HIGH);
+  
+  digitalWrite(D7,LOW);
+  digitalWrite(D8,HIGH);
+}
+
+void left(){
+  server.send(200,"text/html","LEFT");
+  digitalWrite(D5,HIGH);
+  digitalWrite(D6,LOW); 
+  
+  digitalWrite(D7,LOW);
+  digitalWrite(D8,HIGH); 
+}
+
+void right(){
+  server.send(200,"text/html","RIGHT");
+  digitalWrite(D5,LOW);
+  digitalWrite(D6,HIGH); 
+  
+  digitalWrite(D7,HIGH);
+  digitalWrite(D8,LOW);
+}
+
+void setup() {
+ pinMode(D5,OUTPUT);
+ pinMode(D6,OUTPUT);
+ pinMode(D7,OUTPUT);
+ pinMode(D8,OUTPUT);
+ delay(100);
+
+ WiFi.softAPConfig(ip,gateway,subnet);
+ WiFi.softAP(ssid,password);
+
+ server.on("/",handleRoot);
+ server.on("/halt",halt);
+ server.on("/forward",forward);
+ server.on("/backward",backward);
+ server.on("/left",left);
+ server.on("/right",right);
+ server.begin();
+}
+
+void loop() {
+ server.handleClient();
+}
